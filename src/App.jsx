49209bd404914b38f1ee66e3e9c859412abc346b
Prabesh <prabesh5511@gmail.com>
2025-05-21 09:52:56 +0545

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,88 +1,21 @@
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
-// import { supabase } from "./client";
 
-// import "./App.css";
-
-// function App() {
-//   const [formData, setFormData] = useState({
-//     fullName: "",
-//     email: "",
-//     password: "",
-//   });
-//   console.log(formData)
-
-//   function handleChange(event) {
-//     setFormData((prevFormData) => {
-//       return {
-//         ...prevFormData,
-//         [event.target.name]: event.target.value,
-//       };
-//     });
-//   }
-
-//  async function handleSubmit(e){
-//   e.preventDefault()
-//     try{
-//       const { data, error } = await supabase.auth.signUp({
-//         email: formData.email,
-//         password: formData.password,
-//         options:{
-//           data:{
-//             full_name: formData.fullName,
-//           }
-//         }
-//       })
-//       alert('Check your email for verification link.')
-
-//     }catch(err){
-//       alert(err)
-//     }
-//   }
-
-//   return (
-//     <div>
-//       <form onSubmit={handleSubmit}>
-//         <input
-//           type="text"
-//           placeholder="Fullname"
-//           name="fullName"
-//           onChange={handleChange}
-//         />
-//         <input
-//           type="text"
-//           placeholder="Email"
-//           name="email"
-//           onChange={handleChange}
-//         />
-//         <input
-//           type="password"
-//           placeholder="Password"
-//           name="password"
-//           onChange={handleChange}
-//         />
-
-//         <button type="submit">submit</button>
-//       </form>
-//     </div>
-//   );
-// }
-
-// export default App;
 
 import React, { useEffect, useState } from "react";
+//pages folder bata 3ta pages import 
 import { SignUp, Login, Homepage } from "./pages";
+//navigation setup vai nai halyo
 import { Routes, Route } from "react-router-dom";
 // import Homepage from "./pages/Homepage";
 
 const App = () => {
   const [token, setToken] = useState(false)
-
+  //token cha vani :user logged-in hudha khari, login info lagera session storage ma save garne
+  //page reload ma lost na hos vanera
   if(token){
     sessionStorage.setItem('token',JSON.stringify(token))
   }
 
+// check gane ko lagi session storage ma : yedi token cha vani token state lai update garcha.
   useEffect(() => {
 if(sessionStorage.getItem('token')){
   let data = JSON.parse(sessionStorage.getItem('token'))
@@ -96,12 +29,12 @@ if(sessionStorage.getItem('token')){
     <div>
       <Routes>
         <Route path={"/signup"} element={<SignUp/>} />
-        <Route path={"/"} element={<Login setToken={setToken}/>} />
+{/*         settoken le token haru pass garya vai halyo */}
+        <Route path={"/"} element={<Login setToken={setToken}/>} />  
         {token?<Route path={"/homepage"} element={<Homepage token={token}/>} />: ""}
 
       </Routes>
 
-      {/* <SignUp /> */}
     </div>
   );
 };
